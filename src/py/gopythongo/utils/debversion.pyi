d811806d659c1bd4c60f8739e51ef6c1143f7283
jdelic <jonas-bitbucket@maurus.net>
2016-03-15 17:59:42 +0100

    refactor debian version module with new name and subpackage

---
+++ src/py/gopythongo/utils/debversion.pyi
@@ -0,0 +1,30 @@
+# -* coding: utf-8 *-
+
+# Type stub for gopythongo.debian so we're Python2 compatible
+from typing import Tuple, List
+
+
+def debian_substr_compare(a: str, b: str) -> int:
+    pass
+
+
+def split_version_parts(version_str: str, version_re: str) -> List[str]:
+    pass
+
+
+class DebianVersion(object):
+    def __init__(self, epoch: str, version: str, revision: str):
+        pass
+
+    @staticmethod
+    def fromstring(version_str: str) -> DebianVersion:
+        pass
+
+    def as_tuple(self) -> Tuple[str, str, str]:
+        pass
+
+    def __lt__(self, other: DebianVersion) -> bool:
+        pass
+
+    def __eq__(self, other: DebianVersion) -> bool:
+        pass
