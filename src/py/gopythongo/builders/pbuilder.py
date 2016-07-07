55b9be2ff986d232f405abbdef00b18ef7d59a33
jdelic <jonas-bitbucket@maurus.net>
2016-07-07 15:28:05 +0200

    add run-after-create functionality

--- src/py/gopythongo/builders/pbuilder.py
+++ src/py/gopythongo/builders/pbuilder.py
@@ -38,6 +38,11 @@ class PbuilderBuilder(BaseBuilder):
                                       "have to install python using --apt-get, or GoPythonGo will not be able to run "
                                       "inside the container, but this gives you more control about what Python version "
                                       "runs.")
+        gr_pbuilder.add_argument("--run-after-create", dest="pbuilder_run_after_create", action="append",
+                                 help="Specify commands (e.g. shell scripts) which will be run using 'pbuilder "
+                                      "--execute --save-after-exec' after a build environment is created. This allows "
+                                      "you to perform additional necessary build configuration, which shouldn't be "
+                                      "repeated for each subsequent build (e.g. 'gem install fpm')")
         gr_pbuilder.add_argument("--apt-get", dest="build_deps", action="append", default=[],
                                  help="Packages to install using apt-get prior to creating the virtualenv (e.g. driver "
                                       "libs for databases so that Python C extensions compile correctly")
@@ -55,25 +60,33 @@ class PbuilderBuilder(BaseBuilder):
 
     def validate_args(self, args: argparse.Namespace) -> None:
         if args.is_inner:
-            return
+            pass
+        else:
+            # validate things we only need to validate in the outer layer
+            if not os.path.exists(args.pbuilder_executable) or not os.access(args.pbuilder_executable, os.X_OK):
+                raise ErrorMessage("pbuilder not found in path or not executable (%s).\n"
+                                   "You can specify an alternative path using %s" %
+                                   (args.pbuilder_executable, highlight("--use-pbuilder")))
 
-        if not os.path.exists(args.pbuilder_executable) or not os.access(args.pbuilder_executable, os.X_OK):
-            raise ErrorMessage("pbuilder not found in path or not executable (%s).\n"
-                               "You can specify an alternative path using %s" %
-                               (args.pbuilder_executable, highlight("--use-pbuilder")))
+            if args.basetgz and os.path.exists(args.basetgz) and not os.path.isfile(args.basetgz):
+                raise ErrorMessage("pbuilder basetgz %s exists but is not a file. Can't continue with this "
+                                   "inconsistency." % highlight(args.basetgz))
 
-        if args.basetgz and os.path.exists(args.basetgz) and not os.path.isfile(args.basetgz):
-            raise ErrorMessage("pbuilder basetgz %s exists but is not a file. Can't continue with this "
-                               "inconsistency." % highlight(args.basetgz))
+            if not args.pbuilder_distribution:
+                raise ErrorMessage("pbuilder distribution unfortunately defaults to %s, so you must explicitly set it "
+                                   "using the parameter %s" %
+                                   (highlight("sid (unstable)"), highlight("--distribution")))
 
-        if not args.pbuilder_distribution:
-            raise ErrorMessage("pbuilder distribution unfortunately defaults to %s, so you must explicitly set it "
-                               "using the parameter %s" %
-                               (highlight("sid (unstable)"), highlight("--distribution")))
+            if os.getuid() != 0:
+                raise ErrorMessage("pbuilder requires root privileges. Please run GoPythonGo as root when using "
+                                   "pbuilder")
 
-        if os.getuid() != 0:
-            raise ErrorMessage("pbuilder requires root privileges. Please run GoPythonGo as root when using "
-                               "pbuilder")
+            for runspec in args.pbuilder_run_after_create:
+                if os.path.isfile(runspec):
+                    if not os.access(runspec, os.X_OK):
+                        raise ErrorMessage("Pbuilder is supposed to run %s inside the build environment, but it's not "
+                                           "executable" % highlight(runspec))
+                    the_context.mounts.add(os.path.abspath(os.path.dirname(runspec)))
 
     def build(self, args: argparse.Namespace) -> None:
         print_info("Building with %s" % highlight("pbuilder"))
@@ -105,11 +118,6 @@ class PbuilderBuilder(BaseBuilder):
 
             run_process(*create_cmdline)
 
-        if args.pbuilder_debug_login:
-            build_cmdline = [args.pbuilder_executable, "--login"]
-        else:
-            build_cmdline = [args.pbuilder_executable, "--execute"]
-
         mounts = ""
         if args.mounts:
             mounts += " ".join(args.mounts)
@@ -119,20 +127,36 @@ class PbuilderBuilder(BaseBuilder):
                 mounts += " "
             mounts += " ".join(the_context.mounts)
 
+        build_args = []
+        build_args += shlex.split(" ".join(flatten(args.pbuilder_opts)))
+        build_args += shlex.split(" ".join(flatten(args.pbuilder_execute_opts)))
         if mounts:
-            build_cmdline += ["--bindmounts", mounts]
+            build_args += ["--bindmounts", mounts]
 
         if args.basetgz:
-            build_cmdline += ["--basetgz", args.basetgz]
+            build_args += ["--basetgz", args.basetgz]
+
+        for ix, runspec in enumerate(args.pbuilder_run_after_create):
+            print_info("Running post-creation commands for build environment %s of %s" %
+                       (highlight(ix + 1), highlight(str(len(args.pbuilder_run_after_create)))))
+            if os.path.isfile(os.path.abspath(runspec)):
+                runspec = os.path.abspath(runspec)
+            post_create_cmdline = [args.pbuilder_executable, "--execute"] + build_args + \
+                                  ["--save-after-exec", "--", runspec]
+            run_process(*post_create_cmdline)
 
         if args.pbuilder_debug_login:
-            debug_cmdline = build_cmdline + ["--"] + the_context.gopythongo_cmd + ["--inner"] + sys.argv[1:]
+            build_cmdline = [args.pbuilder_executable, "--login"]
+            debug_cmdline = build_cmdline + build_args + ["--"] + the_context.gopythongo_cmd + ["--inner"] + \
+                            sys.argv[1:]
             debug_cmdline = [x if x != "--login" else "--execute" for x in debug_cmdline]
             print_debug("Without --login, GoPythonGo would run: %s" % " ".join(debug_cmdline))
         else:
+            build_cmdline = [args.pbuilder_executable, "--execute"] + build_args
+            # TODO: add version serialization
             build_cmdline += ["--"] + the_context.gopythongo_cmd + ["--inner"] + sys.argv[1:]
 
-        run_process(*build_cmdline)
+        run_process(*build_cmdline, interactive=args.pbuilder_debug_login)
 
 
 builder_class = PbuilderBuilder
