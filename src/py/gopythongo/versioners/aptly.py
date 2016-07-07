bd9ac68f6eda2a626d1c357a87ba1b1c6b13fb6d
jdelic <jonas-bitbucket@maurus.net>
2016-07-08 00:05:16 +0200

    remove all mentions of --package-name which doesn't exist anymore

--- src/py/gopythongo/versioners/aptly.py
+++ src/py/gopythongo/versioners/aptly.py
@@ -31,18 +31,17 @@ class AptlyVersioner(BaseVersioner):
         gr_aptly = parser.add_argument_group("Aptly Versioner options")
         gr_aptly.add_argument("--fallback-version", dest="aptly_fallback_version", default=None,
                               help="If the APT repository does not yet contain a package with the name specified by "
-                                   "--package-name, the Aptly versioner can return a fallback value. This is useful "
+                                   "--aptly-query, the Aptly versioner can return a fallback value. This is useful "
                                    "for fresh repositories.")
         gr_aptly.add_argument("--aptly-versioner-opts", dest="aptly_versioner_opts", default=[],
                               help="Specify additional command-line parameters which will be appened to every "
                                    "invocation of aptly by the Aptly Versioner.")
         gr_aptly.add_argument("--aptly-query", dest="aptly_query", default=None,
-                              help="By default the aptly Versioner will return the latest version of the package "
-                                   "identified by --package-name in the repo. But sometimes you don't want that, but "
-                                   "would like to, for example, get the latest revision of a specific version. You "
-                                   "can do that by using --aptly-query, which will override the default. (Example: "
-                                   "--aptly-query='Name (aptly), Version (>0.9.5), Version (<=0.9.6)'). More "
-                                   "information on the query syntax can be found on https://aptly.info.")
+                              help="Set the query to run on the aptly repo. For example: get the latest revision of a "
+                                   "specific version through --aptly-query='Name ([yourpackage]), Version (>0.9.5), "
+                                   "Version (<=0.9.6)'). More information on the query syntax can be found on "
+                                   "https://aptly.info. To find the overall latest version of GoPythonGo in a repo, "
+                                   "you would use --aptly-query='Name (gopythongo)'")
 
     def validate_args(self, args: argparse.Namespace) -> None:
         _aptly_args.validate_shared_args(args)
@@ -54,8 +53,8 @@ class AptlyVersioner(BaseVersioner):
                 raise ErrorMessage("The fallback version string you specified via %s is not a valid Debian version "
                                    "string. (%s)" % (highlight("--fallback-version"), str(e))) from e
 
-        if not args.aptly_query and not args.package_name:
-            raise ErrorMessage("To use the Aptly Versioner, you must specify --package-name or --aptly-query.")
+        if not args.aptly_query:
+            raise ErrorMessage("To use the Aptly Versioner, you must specify --aptly-query.")
 
     def read(self, args: argparse.Namespace) -> str:
         cmd = _aptly_args.get_aptly_cmdline(args)
@@ -63,8 +62,7 @@ class AptlyVersioner(BaseVersioner):
         if args.aptly_versioner_opts:
             cmd += args.aptly_versioner_opts
 
-        package_query = args.aptly_query if args.aptly_query else args.package_name
-        cmd += ["repo", "search", "-format=\"{{.Version}}\"", args.aptly_repo, package_query]
+        cmd += ["repo", "search", "-format=\"{{.Version}}\"", args.aptly_repo, args.aptly_query]
 
         output = run_process(*cmd).split("\n")
         if output == "ERROR: no results":
@@ -72,7 +70,7 @@ class AptlyVersioner(BaseVersioner):
                 return args.fallback_version
             else:
                 raise ErrorMessage("The aptly Versioner was unable to find any packages ('ERROR: no results') matching "
-                                   "the package-name or query.")
+                                   "the query %s." % highlight(args.aptly_query))
         else:
             versions = []  # type: List[DebianVersion]
             for line in output:
