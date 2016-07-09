fd1c32819ee277c4bec0b989d88761c07cfc8e91
jdelic <jonas-bitbucket@maurus.net>
2016-07-09 14:24:25 +0200

    fix up the FPM Packer to a working state

--- src/py/gopythongo/packers/fpm.py
+++ src/py/gopythongo/packers/fpm.py
@@ -1,6 +1,7 @@
 # -* encoding: utf-8 *-
 import argparse
 import json
+import shlex
 
 import os
 
@@ -58,7 +59,7 @@ class FPMPacker(BasePacker):
         gr_opts = parser.add_argument_group("FPM related options (can also be used in OPTS_FILE):")
         gr_opts.add_argument("--fpm-format", dest="fpm_format", choices=["deb"], default="deb",
                              help="Output package format. Only 'deb' is supported for now")
-        gr_opts.add_argument("--fpm-opts", dest="fpm_opts", action="append", default=[],
+        gr_opts.add_argument("--fpm-opts", dest="fpm_extra_opts", action="append", default=[],
                              help="Any string specified here will be directly appended to the FPM command-line when it "
                                   "is invoked, allowing you to specify arbitrary extra command-line parameters. Make "
                                   "sure that you use an equals sign, i.e. --fpm-opts='' to avoid 'Unknown parameter' "
@@ -70,24 +71,28 @@ class FPMPacker(BasePacker):
                                "You can specify an alternative executable using %s" %
                                (args.fpm, highlight("--use-fpm")))
 
-        if args.fpm_opts:
-            for opts in args.fpm_opts:
+        if args.run_fpm:
+            for opts in args.run_fpm:
+                if opts.startswith("template:"):
+                    opts = opts[len("template:"):]
                 if not os.path.exists(opts) or not os.access(opts, os.R_OK):
                     raise ErrorMessage("It seems that fpm will not be able to read %s under the user id that "
                                        "GoPythonGo is running under." % opts)
 
-    def _read_fpm_opts_from_file(self, optsfile: str, ctx: Dict[str, Any]) -> List[str]:
+    def _read_fpm_opts_from_file(self, optsfile: str, ctx: Dict[str, Any], *,
+                                 process_templates: bool=True) -> List[str]:
         with open(optsfile, mode="rt", encoding="utf-8") as f:
             opts = f.readlines()
 
-        for ix in range(0, len(opts)):
-            opts[ix] = opts[ix].strip()
-            pswt = template.parse_template_prefixes(opts[ix])
-            if pswt:
-                tplfn = []  # type: List[str]
-                for tpl in pswt.templates:
-                    tplfn.append(template.process_to_tempfile(tpl, ctx))
-                opts[ix] = pswt.format_str.format(*tplfn)
+        if process_templates:
+            for ix in range(0, len(opts)):
+                opts[ix] = opts[ix].strip()
+                pswt = template.parse_template_prefixes(opts[ix])
+                if pswt:
+                    tplfn = []  # type: List[str]
+                    for tpl in pswt.templates:
+                        tplfn.append(template.process_to_tempfile(tpl, ctx))
+                    opts[ix] = pswt.format_str.format(*tplfn)
 
         return opts
 
@@ -103,13 +108,13 @@ class FPMPacker(BasePacker):
 
         return self._read_fpm_opts_from_file(thefile, ctx)
 
-    def _run_fpm(self, args: argparse.Namespace) -> None:
+    def pack(self, args: argparse.Namespace) -> None:
         # TODO: don't use deb here, fpm works universally
         fpm_base = [
             args.fpm, "-t", "deb", "-s", "dir",
         ]
 
-        fpm_base += args.fpm_opts
+        fpm_base += args.fpm_extra_opts
 
         ctx = {
             "basedir": args.build_path,
@@ -117,24 +122,28 @@ class FPMPacker(BasePacker):
             "debian_version": the_context.out_version.convert_to("debian")  # FPM requires a Debian version for .debs
         }
 
-        for ix in range(args.run_fpm):
-            print_info("Running FPM run %s of %s" % (ix, len(args.run_fpm)))
-            processed_args = self._load_fpm_opts(args.run_fpm[ix], ctx)
-            parsed_args = self._get_fpm_opts_parser().parse_known_args(processed_args)
+        for ix, fpm_opts in enumerate(args.run_fpm):
+            print_info("Running FPM run %s of %s" % (ix + 1, len(args.run_fpm)))
+            processed_args = self._load_fpm_opts(fpm_opts, ctx)
+            parsed_args, _ = self._get_fpm_opts_parser().parse_known_args(processed_args)
 
-            if not parsed_args.package_name:
-                raise ErrorMessage("FPM opts file MUST contain a package name (-n|--name) parameter (%s)" %
-                                   args.run_fpm[ix])
+            if parsed_args.package_file:
+                package_file = parsed_args.package_file.strip()
+                if os.path.exists(package_file):
+                    print_info("%s already exists, will be removed and recreated" % (highlight(package_file)))
+                    os.unlink(package_file)
 
-            run_params = fpm_base + processed_args
+            run_params = fpm_base
+            for argline in processed_args:
+                run_params += shlex.split(argline)
             fpm_out = run_process(*run_params)
 
             out_file = ""
             if parsed_args.package_file:
-                if not os.path.exists(args.package_file):
+                if not os.path.exists(package_file):
                     raise ErrorMessage("File not found: %s expected to exist from parsed FPM opts" %
-                                       highlight(args.package_file))
-                out_file = parsed_args.package_file
+                                       highlight(parsed_args.package_file))
+                out_file = package_file
             else:
                 parsed_output = self._parse_fpm_output(fpm_out)
                 if "path" in parsed_output:
@@ -145,7 +154,9 @@ class FPMPacker(BasePacker):
                 else:
                     raise ErrorMessage("Running FPM did not result in an output file. Unable to find a %s property in "
                                        "FPM's output or an output filename parameter (-p) in the FPM opts file (%s)" %
-                                       (highlight(":path"), highlight(args.run_fpm[ix])))
+                                       (highlight(":path"), highlight(fpm_opts)))
+
+            print_info("FPM: %s created" % highlight(out_file))
 
             the_context.packer_artifacts.add(PackerArtifact(
                 "deb",
@@ -154,9 +165,6 @@ class FPMPacker(BasePacker):
                 self,
                 self.packer_name
             ))
-
-    def pack(self, args: argparse.Namespace) -> None:
-        self._run_fpm(args)
 
 
 packer_class = FPMPacker
