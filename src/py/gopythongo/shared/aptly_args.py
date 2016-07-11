32ce1693ebf8faec5548df28997d5257caf1bf4d
jdelic <jonas-bitbucket@maurus.net>
2016-07-11 13:50:19 +0200

    require --repo for aptly modules

--- src/py/gopythongo/shared/aptly_args.py
+++ src/py/gopythongo/shared/aptly_args.py
@@ -20,8 +20,7 @@ def add_shared_args(parser: argparse.ArgumentParser) -> None:
         gr_aptly_shared.add_argument("--aptly-config", dest="aptly_config", default=None,
                                      help="Path to the aptly config file to use")
         gr_aptly_shared.add_argument("--repo", dest="aptly_repo", default=None,
-                                     help="Name of the aptly repository to place the package in. (This must be "
-                                          "accessible from the builder environment to be useful.)")
+                                     help="Name of the aptly repository to place the package in.")
 
     _aptly_shared_args_added = True
 
@@ -31,6 +30,10 @@ def validate_shared_args(args: argparse.Namespace) -> None:
         raise ErrorMessage("aptly not found in path or not executable (%s). You can specify "
                            "an alternative path using %s" %
                            (args.aptly_executable, highlight("--use-aptly")))
+
+    if not args.aptly_repo:
+        raise ErrorMessage("To use the aptly Versioner or Store, you MUST provide the name of the aptly repository to "
+                           "operate on via %s" % highlight("--repo"))
 
 
 def get_aptly_cmdline(args: argparse.Namespace) -> List[str]:
