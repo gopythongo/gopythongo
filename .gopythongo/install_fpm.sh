1f74b36e445ff14addc47ccc062a45ed7a9e435a
jdelic <jonas-bitbucket@maurus.net>
2016-07-14 21:35:43 +0200

    MILESTONE: commit gopythongo's gopythongo config folder to gopythongo

---
+++ .gopythongo/install_fpm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# do nothing if fpm already exists
+test -e /usr/local/bin/fpm && exit 0
+
+EATMYDATA=""
+if test -e /usr/bin/eatmydata; then
+    EATMYDATA="/usr/bin/eatmydata"
+fi
+
+# make sure we have gem
+if ! test -e /usr/bin/gem; then
+    $EATMYDATA apt-get update
+    $EATMYDATA apt-get --no-install-recommends -y install ruby ruby-dev
+fi
+
+$EATMYDATA gem install fpm
