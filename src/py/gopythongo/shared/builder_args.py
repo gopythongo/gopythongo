b1d05876b623e7981ab90dad533fd1cacfeb194b
jdelic <jonas-bitbucket@maurus.net>
2016-07-28 10:36:26 +0200

    move some pbuilder options back from the shared module, since I opt for another approach with docker

--- src/py/gopythongo/shared/builder_args.py
+++ src/py/gopythongo/shared/builder_args.py
@@ -18,20 +18,11 @@ def add_args(parser: configargparse.ArgumentParser) -> None:
         _args_added = True
 
     gr_sb = parser.add_argument_group("Common Builder options")
-    gr_sb.add_argument("--no-install-defaults", dest="install_defaults", action="store_false",
-                       default=True,
-                       help="By default GoPythonGo will always install python, python-virtualenv, python-pip, "
-                            "python[3]-dev, virtualenv and possibly eatmydata. If you set this flag you will have to "
-                            "install python using --apt-get, or GoPythonGo will not be able to run inside the "
-                            "container, but this gives you more control about what Python version runs.")
     gr_sb.add_argument("--run-after-create", dest="run_after_create", action="append",
                        help="Specify commands (e.g. shell scripts) which will be run using inside a build environment "
                             "e.g. pbuilder or docker  after a build environment is created. This allows you to perform "
                             "additional necessary build configuration, which shouldn't be repeated for each subsequent "
                             "build (e.g. 'gem install fpm')")
-    gr_sb.add_argument("--apt-get", dest="build_deps", action="append", default=[],
-                       help="Packages to install using apt-get prior to creating the virtualenv (e.g. driver libs for "
-                            "databases so that Python C extensions compile correctly")
 
 
 def validate_args(args: configargparse.Namespace) -> None:
