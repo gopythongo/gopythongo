866d42d260a9f71c40813fa82304f375440d04a3
Jonas Maurus <jonas-bitbucket@maurus.net>
2016-09-13 13:06:01 +0200

    simple implementation of certifybuild assembler

--- src/py/gopythongo/assemblers/certifybuild.py
+++ src/py/gopythongo/assemblers/certifybuild.py
@@ -1,6 +1,9 @@
 # -* encoding: utf-8 *-
 import configargparse
+
 from gopythongo.assemblers import BaseAssembler
+from gopythongo.utils import create_script_path, run_process, cmdargs_unquote_split
+from gopythongo.utils.buildcontext import the_context
 
 
 class CertifyBuildAssembler(BaseAssembler):
@@ -12,8 +15,18 @@ class CertifyBuildAssembler(BaseAssembler):
     def assembler_type(self) -> str:
         return BaseAssembler.TYPE_PREISOLATION
 
-    def assemble(self, args: configargparse.Namespace) -> None:
+    def add_args(self, parser: configargparse.ArgumentParser) -> None:
+        gp_cert = parser.add_argument_group("Certify build options")
+        gp_cert.add_argument("--vaultgetcert-opts", dest="vaultgetcert_opts", default="",
+                             help="Specify arguments for the execution of vaultgetcert.")
+
+    def validate_args(self, args: configargparse.Namespace) -> None:
         pass
+
+    def assemble(self, args: configargparse.Namespace) -> None:
+        cmdargs = [create_script_path(the_context.gopythongo_path, "vaultgetcert")]
+        cmdargs += cmdargs_unquote_split(args.vaultgetcert_opts)
+        run_process(*cmdargs)
 
     def print_help(self) -> None:
         print("Certify build Assembler\n"
