4de2763be992933a2bb993ccbf0b665ae12c7285
Jonas Maurus <jonas-github@maurus.net>
2017-03-23 00:06:31 +0100

    bump version

--- src/py/gopythongo/__init__.py
+++ src/py/gopythongo/__init__.py
@@ -4,5 +4,5 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-version = "0.6.11.dev0"
+version = "0.6.12.dev0"
 program_version = "GoPythonGo %s" % version
