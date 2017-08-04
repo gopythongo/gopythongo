adf84c5010616830853f8b8d190aade12dbe94f8
Jonas Maurus <jonas-github@maurus.net>
2017-08-05 00:14:34 +0200

    bump aptly-api-client dependency to 0.1.3

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ _requirements = [
     'dockerpty==0.4.1',
     'pyopenssl==17.2.0',
     'bumpversion==0.5.3',
-    'aptly-api-client==0.1.1',
+    'aptly-api-client==0.1.3',
 ]
 
 if sys.version_info.major < 3 or (sys.version_info.major == 3 and sys.version_info.minor < 3):
