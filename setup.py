dcf723a1f37fc1c4a45d52b0d4fde7aeb146bced
requires.io <support@requires.io>
2018-02-02 23:21:19 +0100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ _requirements = [
     'semantic-version==2.6.0',
     'packaging==16.8',
     'typing==3.6.4',
-    'hvac==0.3.0',
+    'hvac==0.4.0',
     'docker-py==1.10.6',
     'dockerpty==0.4.1',
     'pyopenssl==17.5.0',
