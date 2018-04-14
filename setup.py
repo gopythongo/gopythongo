62a42dc97a27273466790ca328fff9981ba472cb
Jonas Maurus <jonas-github@maurus.net>
2018-04-14 19:06:27 +0200

    bump aptly api client version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ _requirements = [
     'dockerpty==0.4.1',
     'pyopenssl==17.5.0',
     'bumpversion==0.5.3',
-    'aptly-api-client==0.1.4',
+    'aptly-api-client==0.1.6',
 ]
 
 if sys.version_info.major < 3 or (sys.version_info.major == 3 and sys.version_info.minor < 3):
