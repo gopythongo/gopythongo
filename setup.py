1ec392e54a14dde3e56c30a4ebc687fe3a084645
requires.io <support@requires.io>
2019-11-21 13:19:15 +0100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ _requirements = [
     'Sphinx==2.2.1',
     'sphinx-rtd-theme==0.4.3',
     'colorama==0.4.1',
-    'semantic-version==2.8.1',
+    'semantic-version==2.8.3',
     'packaging==19.2',
     'typing==3.7.4.1',
     'hvac==0.9.6',
