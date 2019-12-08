45ac51ffbfddbc49f1c819c1d3b0eab7ede25d58
requires.io <support@requires.io>
2019-12-08 17:33:36 +0100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ _packages = find_packages(_package_root, exclude=["*.tests", "*.tests.*", "tests
 
 _requirements = [
     'Jinja2==2.10.3',
-    'ConfigArgParse==0.15.1',
+    'ConfigArgParse==0.15.2',
     'Sphinx==2.2.2',
     'sphinx-rtd-theme==0.4.3',
     'colorama==0.4.3',
