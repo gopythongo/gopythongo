b1b27701bef975761cf115a0bd021fc3e70df8f4
requires.io <support@requires.io>
2020-06-17 18:13:01 +0200

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ _requirements = [
     'Jinja2==2.11.2',
     'ConfigArgParse==1.2.3',
     'Sphinx==3.1.1',
-    'sphinx-rtd-theme==0.4.3',
+    'sphinx-rtd-theme==0.5.0',
     'colorama==0.4.3',
     'semantic-version==2.8.5',
     'packaging==20.4',
